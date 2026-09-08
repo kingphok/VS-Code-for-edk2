f92326b4a025d37e06b1380e5c0b2d0d094d5612
kingphok <kingphok@gmail.com>
2026-09-09 00:42:32 +0800

    [GoToDefinition] Adds empty funciotn for build database for F12 Go to definition.
    DatabaseIndexing() - Index all files for go to definition database
    DatabaseWatching() - Watch file and index it for go to definition database

--- src-ts/extension.ts
+++ src-ts/extension.ts
@@ -2,6 +2,24 @@ import * as vscode from 'vscode';
 import { symbolProvider_OutlineSection } from './outline.edk2-section';
 import { symbolProvider_OutlineVarid } from './outline.vfr-varid';
 
+async function DatabaseIndexing(context: vscode.ExtensionContext) {
+    const startTime = performance.now();
+
+    // Index all files for go to definition database
+
+    const endTime = performance.now();
+    console.log(`[EDK2] DatabaseIndexing: ${(endTime - startTime).toFixed(2)} ms`);
+}
+
+function DatabaseWatching(context: vscode.ExtensionContext) {
+    const startTime = performance.now();
+
+    // Watch file and index it for go to definition database
+
+    const endTime = performance.now();
+    console.log(`[EDK2] DatabaseWatching: ${(endTime - startTime).toFixed(2)} ms`);
+}
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('[EDK2] Extension has been successfully compiled and activated!');
 
@@ -9,6 +27,9 @@ export function activate(context: vscode.ExtensionContext) {
         symbolProvider_OutlineSection(),
         symbolProvider_OutlineVarid()
     );
+
+    DatabaseIndexing(context);
+    DatabaseWatching(context);
 
     //    vscode.window.showInformationMessage('[EDK2] Extension has been successfully compiled and activated!');
 }
