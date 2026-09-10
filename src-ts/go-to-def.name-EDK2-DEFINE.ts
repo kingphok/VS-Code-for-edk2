a29073185427b8f11c749cd1d44b8eaae6274f84
kingphok <kingphok@gmail.com>
2026-09-10 13:13:15 +0800

    [GoToDefinition] Get word of $(marco_name) for EDK2 DEFINE when pressing F12 or Ctrl key.

--- src-ts/go-to-def.name-EDK2-DEFINE.ts
+++ src-ts/go-to-def.name-EDK2-DEFINE.ts
@@ -7,12 +7,21 @@ const targetLanguages_EDK2_DEFINE = [
     { language: 'edk2inf' }
 ];
 
+// Caller file syntax: $($(marco_name))
+const EDK2_DEFINE_CALLER_REGEX = /\$\(([a-zA-Z_][a-zA-Z0-9_]*)\)/;
+
 class main_edk2DefineProvider implements vscode.DefinitionProvider {
     provideDefinition(
         document: vscode.TextDocument,
         position: vscode.Position
     ): vscode.Location | vscode.Location[] | null {
-        console.log('[EDK2] EDK2 DEFINE');
+
+        const range = document.getWordRangeAtPosition(position, EDK2_DEFINE_CALLER_REGEX);
+        if (!range) return null;
+        const rawText = document.getText(range);
+        const word = rawText.slice(2, -1);
+
+        console.log(`[EDK2] EDK2 DEFINE: ${word}`);
         return null;
     }
 }
